ce0e255f469b270b72f8262453cdeeba07194de7
Manu <manuelass.dev@gmail.com>
2024-07-18 20:27:09 -0300

    Inicio do Projeto

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Button from '../src/components/Button/Button';
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <h1>Home Page!</h1>
+      <Button>
+        Botão
+      </Button>
+    </div>
+  )
+}
